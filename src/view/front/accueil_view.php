479aef0b377dce01626bf45de5d5d0e705c0da7d
matthieuClio <makabay@hotmail.fr>
2019-09-17 21:36:34 +0200

    Projet5-V-0.1
    Modification du CSS, Ajout d'une favicone, modification de la première partie du site (accueil).
    Création de la deuxième partie du site (présentation).
    
    Céation de la troisième partie du site (compétences).

--- src/view/front/accueil_view.php
+++ src/view/front/accueil_view.php
@@ -6,8 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Portfolio Matthieu</title>
 		<meta name="description" content="Le livre Billet simple pour l'Alaska est disponible en lecture gratuite sur ce site.">
-		<link rel="icon" type="image/jpg" href="public/images/icone/book.jpg">
-		<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+		<link rel="icon" type="image/jpg" href="public/images/icone/keyboard.png">
+		<!-- <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"> -->
 		<link rel="stylesheet" href="public/css/style.css">
 	</head>
 
@@ -16,14 +16,48 @@
 		<nav class="menu">
 			<ul>
 				<li>
+					ACCUEIL
+				</li>
+
+				<li>
 					COMPETENCES
 				</li>
 			</ul>
 		</nav>
 
-		<main>
-			Test
-		</main>
+		<header class="slider">
+			<img src="public/images/photo/dev_web.jpg" alt="ordinateur" class="slider_photo">
+			
+			<figure class="slider_picture">
+				<img src="public/images/photo/cv_picture.png">
+			</figure>
+
+			<section class="slider_information">
+				<h1>Développeur web</h1>
+				<p class="slider_text_one">
+					Info 1
+				</p>
+
+				<p class="slider_text_two">
+					Info 2
+				</p>
+			</section>
+		</header>
+
+		<section class="history">
+			<h2>Matthieu CLIO</h2>
+
+			<p>Résumé</p>
+
+			<div class="history_background">
+			</div>
+		</section>
+
+		<section class="skill">
+			<h2>Compétences</h2>
+
+			<p>Liste compétences</p>
+		</section>
 
 		<footer>
 		</footer>
